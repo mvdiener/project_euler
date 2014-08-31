828f63ccd6278a54158c9a12b8e3b28f1fea9316
Max Diener <mvdiener@gmail.com>
2014-08-30 20:56:48 -0500

    Slight refactor

--- problem048.rb
+++ problem048.rb
@@ -2,4 +2,4 @@
 
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
-p (1..1000).inject{|sum, num| sum + num**num}.to_s.split('')[-10..-1].join.to_i
+p (1..1000).inject{|sum, num| sum + num**num}.to_s[-10..-1].to_i

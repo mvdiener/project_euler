2784a18509fe1943b06fba83bc47423b2c1f92d6
Max Diener <mvdiener@gmail.com>
2014-08-31 13:45:32 -0500

    Time <1s

--- Ruby/problem010.rb
+++ Ruby/problem010.rb
@@ -2,23 +2,42 @@
 
 # Find the sum of all the primes below two million.
 
-def is_prime?(number)
-	is_prime = true
-	2.upto(Math.sqrt(number).floor) do |num|
-		if number % num == 0
-			is_prime = false
-			break
+# def is_prime?(number)
+# 	is_prime = true
+# 	2.upto(Math.sqrt(number).floor) do |num|
+# 		if number % num == 0
+# 			is_prime = false
+# 			break
+# 		end
+# 	end
+# 	is_prime
+# end
+
+# def find_primes(limit)
+# 	primes_array = []
+# 	(2...limit).each do |x|
+# 		primes_array << x if is_prime?(x)
+# 	end
+# 	primes_array
+# end
+
+# puts find_primes(2000000).inject(:+)
+
+# using the Sieve of Eratosthenes
+def find_primes(limit)
+	num_array = (2...limit).to_a
+	num_array.each_with_index do |val, index|
+		if val
+			index += val
+			while index < limit-2
+				num_array[index] = nil
+				index += val
+			end
+		else
+			next
 		end
 	end
-	is_prime
+	num_array
 end
 
-def find_primes(limit)
-	primes_array = []
-	(2...limit).each do |x|
-		primes_array << x if is_prime?(x)
-	end
-	primes_array
-end
-
-puts find_primes(2000000).inject(:+)
+p find_primes(2000000).compact.inject(:+)

202f834439a610160d7ab4115d9a8e1e0d7f3362
Maximus Prime <mvdiener@gmail.com>
2017-09-26 15:22:27 -0700

    Create problem004.cs

---
+++ CSharp/problem004.cs
@@ -0,0 +1,37 @@
+//A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+using System;
+using System.Linq;
+
+namespace Problem004
+{
+   class MainClass
+   {
+      public static void Main()
+      {
+         Console.WriteLine(GetPalindromes());
+      }
+
+      public static int GetPalindromes()
+      {
+         int product = 0;
+         int largest = 0;
+         for (int i = 999; i > 100; i--)
+         {
+            for (int j = 999; j > 100; j--)
+            {
+               product = i * j;
+               if (product < largest) break;
+               if (product.ToString() == new string(product.ToString().ToCharArray().Reverse().ToArray()))
+               {
+                  if (product > largest) largest = product;
+                  break;
+               }
+            }
+         }
+         return largest;
+      }
+   }
+}
